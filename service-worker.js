d5cb4a048440c094f8bfd144b90bf30add2894b9
PurushothMathav <purushoth.mathav@gmail.com>
2025-06-26 10:11:53 +0530

    v2606251000

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kukufm-v2506252300';
+const CACHE_NAME = 'kukufm-v2606251000';
 const STATIC_CACHE = `${CACHE_NAME}-static`;
 const DYNAMIC_CACHE = `${CACHE_NAME}-dynamic`;
 const UPDATE_CHECK_INTERVAL = 30000; // Check for updates every 30 seconds
