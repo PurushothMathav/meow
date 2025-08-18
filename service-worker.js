9c9fba49890e702486f6882c1cd9394c7fcfa042
PurushothMathav <purushoth.mathav@gmail.com>
2025-08-18 17:48:46 +0530

    v1808251330

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Optimized Service Worker for Meow Streaming Site
 // service-worker.js
 
-const CACHE_VERSION = 'v1808251320';
+const CACHE_VERSION = 'v1808251330';
 const CACHE_NAMES = {
   STATIC: `meow-static-${CACHE_VERSION}`,
   DYNAMIC: `meow-dynamic-${CACHE_VERSION}`,
