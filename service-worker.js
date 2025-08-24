cf42c160afcdb8138a86ed310092752cafbc095b
PurushothMathav <purushoth.mathav@gmail.com>
2025-08-24 12:34:51 +0530

    v2408251030
    -Loading optimize
    -Bug fixed

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Optimized Service Worker for Meow Streaming Site
 // service-worker.js
 
-const CACHE_VERSION = 'v2408250930';
+const CACHE_VERSION = 'v2408251030';
 const CACHE_NAMES = {
   STATIC: `meow-static-${CACHE_VERSION}`,
   DYNAMIC: `meow-dynamic-${CACHE_VERSION}`,
